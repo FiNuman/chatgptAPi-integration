170798a291519e9f663eec10f2b0d4a5f4f35890
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-06-06 23:26:19 +0600

    sdasd

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
 //========================================================================================
 const express = require("express");
 const app = express();
+const axios = require('axios');
 
-fetch('https://script.google.com/macros/s/AKfycbx4PnSnZC4Q5AdRBbDOuda8Yzb2672jPI2KlYybFWuRlKp9tasmUt5Z2nT9XqecGPMlqg/exec?fullname=numan&value=newdata', {
-   method: 'get',
+
+
+app.get('/', (req, res) => {
+   console.log(req.url)
+   axios.get('https://script.google.com/macros/s/AKfycbx4PnSnZC4Q5AdRBbDOuda8Yzb2672jPI2KlYybFWuRlKp9tasmUt5Z2nT9XqecGPMlqg/exec?fullname=numan&value=newdata')
+      .then(response => {
+         let priviousQuery = response.data.data[0].reqandres;
+
+      })
+      .catch(error => {
+         console.error('Error fetching data:', error);
+      });
 })
-   .then(response => { return response.json() })
-   .then(data => { console.log(data) })
-   .catch(err => { console.error(err) })
- 
-app.get('/',(req,res)=>{
-   res.render('')
-})
- 
- 
+
+
 
 //========================================================================================
 //                                Server Start
 //========================================================================================
 app.listen(process.env.PORT || 80, () => {
-    console.log(`Running`);
+   console.log(`Running`);
 })
