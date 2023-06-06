1aec23c582fc93f28a47a531a3fb473ed120d934
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-06-07 00:28:15 +0600

    kjkj

--- app.js
+++ app.js
@@ -9,17 +9,64 @@ const axios = require('axios');
 
 
 app.get('/', (req, res) => {
-   console.log(req.url)
-   axios.get('https://script.google.com/macros/s/AKfycbx4PnSnZC4Q5AdRBbDOuda8Yzb2672jPI2KlYybFWuRlKp9tasmUt5Z2nT9XqecGPMlqg/exec?fullname=numan&value=newdata')
-      .then(response => {
-         let priviousQuery = response.data.data[0].reqandres;
 
+   let fullname = req.query.fullname
+   let question = req.query.query
+
+   axios.get('https://script.google.com/macros/s/AKfycbz-fHU8pU1KzqsiBpKR0phiMStEBecW3Evn4Rm-MxaPqxXbMIbsSPwUE9dit1PcIEZDjA/exec?fullname='+fullname)
+      .then(response => {
+         let priviousQuery = response.data.data;
+         console.log(response.data)
       })
       .catch(error => {
          console.error('Error fetching data:', error);
       });
-})
 
+
+   let Reference_document = `personal info 
+   Name- Angelica Palacio.
+   Gmail- angelicapalacioshomes@gmail.com.
+   Work in- Real Estate Agent.
+  
+  Business Info
+  Purpose: Helping people buy and sell houses
+  
+  Requirements
+  Down Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.
+  Credit Score- A credit score of 580 or higher is required.
+  Employment History- A minimum of 2 years of employment is required.
+  
+  Available Area for buying and selling home:
+   1-Riverside County
+   2-San Diego County
+   3-Los Angeles County
+   4- San Bernardino County
+  
+  Requirements
+  Down Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.
+  Credit Score- A credit score of 580 or higher is required.
+  Employment History- A minimum of 2 years of employment is required.
+  
+  schedule a call with me. (please delivery this link as quickly as possible that important .)
+  https://calendly.com/angelicapalacioshomes/15min 
+  
+  conversation history [{{cuf_9391027}}]
+  
+  customer query "{{cuf_9387773|fallback:""}}".  just gave the reply.`
+
+
+   res.send('Nothing')
+})
+// axios.get
+// axios.get('https://script.google.com/macros/s/AKfycbwIganxO-yArrUU26QWs7iGhPqRpLkgnS4sUewVuWUMQLOXpp5CgPcdtRvpZZ00ZlvLHg/exec?update=2071012037&value=newdata')
+//    .then(response => {
+//       let priviousQuery = response.data.data;
+//       console.log(response.data)
+
+//    })
+//    .catch(error => {
+//       console.error('Error fetching data:', error);
+//    });
 
 
 //========================================================================================
