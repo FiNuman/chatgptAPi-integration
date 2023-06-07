3e732bc80008a83cf7ad85abf32811b6fb8d0584
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-06-07 21:01:20 +0600

    sas

--- app.js
+++ app.js
@@ -7,92 +7,44 @@ const app = express();
 const axios = require('axios');
 const { Configuration, OpenAIApi } = require("openai");
 
-app.get('/', async (req, res) => {
+
+app.get('/', async(req, res) => {
 
    let fullname = req.query.fullname
    let question = req.query.query
+
    axios.get('https://script.google.com/macros/s/AKfycbz1Ggu7kyUmiGOwYjbwFQrJeFtXfmkETt0s_rfD278eLync2_sC071Eh6mN9_HpYMYeWw/exec?fullname=' + fullname)
       .then(async response => {
          let priviousQuery = response.data.data[0].reqandres;
 
+         console.log(priviousQuery)
          let Reference_document =
-            `
-            personal info:
-            Name Angelica Palacio.
-            Gmail angelicapalacioshomes@gmail.com.
-            Work in Real Estate Agent.
-            
-            Business Info:
-            Purpose Helping people buy and sell houses
-            
-            Requirements:
-            The first-time home buyer program you mentioned requires a 3.5% down payment.
-            A credit score of 580 or higher is required.
-            A minimum of 2 years of employment is required.
-            
-            
-            Available Area for buying and selling home:
-            1 Riverside County
-            2 San Diego County
-            3 Los Angeles County
-            4 San Bernardino County
-            
-            schedule a call with me.
-            https://calendly.com/angelicapalacioshomes/15min please provide the link as soon as possible in conversation That is important.
-           
-           conversation history: [${priviousQuery}]
-           
-
-
-           customer query: "${question}".  just gave the reply dont add this word answer,reply,respone. that is continious chat`
+            `personal info\nName- Angelica Palacio.\nGmail- angelicapalacioshomes@gmail.com.\nWork in- Real Estate Agent.\n\nBusiness Info\nPurpose: Helping people buy and sell houses\n\nRequirements\nDown Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.\nCredit Score- A credit score of 580 or higher is required.\nEmployment History- A minimum of 2 years of employment is required.\n\nAvailable Area for buying and selling home:\n1-Riverside County\n2-San Diego County\n3-Los Angeles County\n4- San Bernardino County\n\nRequirementsDown Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.\nCredit Score- A credit score of 580 or higher is required.\nEmployment History- A minimum of 2 years of employment is required.\n\nschedule a call with me. please delivery this link as quickly as possible in the conversation.)\nhttps://calendly.com/angelicapalacioshomes/15min \n\nconversation history [${priviousQuery}]\n\ncustomer query "${question}".  just gave the reply.`
 
 
          const configuration = new Configuration({
-            apiKey: process.env.gptkey,
+            apiKey: process.env.OPENAI_API_KEY,
          });
          const openai = new OpenAIApi(configuration);
 
          const sda = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: Reference_document,
-            temperature: 0.8,
-            max_tokens: 150,
+            temperature: 0.9,
+            max_tokens: 200,
             top_p: 1,
             frequency_penalty: 0.0,
             presence_penalty: 0.6,
             stop: [" Human:", " AI:"],
          });
- 
-         let text = sda.data.choices[0].text;
-
-         const chunkSize = 255;
-         const chunks = [];
-         for (let i = 0; i < text.length; i += chunkSize) {
-            chunks.push(text.substr(i, chunkSize));
-         }
-         let response1, response2, response3
-         if (chunks.length === 1) {
-            response1 = chunks[0];
-            response2 = ''
-            response3 = ''
-         }else if (chunks.length === 2) {
-            response1 = chunks[1];
-            response2 = chunks[2];
-            response3 = ''
-         }else if (chunks.length === 3) {
-            response1 = chunks[0];
-            response2 = chunks[1];
-            response3 = chunks[2];
-         }
-         console.log({ response1: response1, response2: response2, response3: response3 })
-         res.json({ response1: response1, response2: response2, response3: response3 })
+         console.log(sda.data.choices[0].text)
+         res.json({response : sda.data.choices[0].text})
       })
       .catch(error => {
          console.error('Error fetching data:', error);
       });
 })
 
-// res.json({ response1: response1, response2: response2, response3: response3 })
 
 //========================================================================================
 //                                Server Start
