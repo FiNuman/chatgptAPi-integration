469f90db40a498fa3764fd6f77f40eb87bd5db19
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2023-06-07 19:41:43 +0600

    dasdasdsa

--- app.js
+++ app.js
@@ -14,9 +14,63 @@ app.get('/', async (req, res) => {
    axios.get('https://script.google.com/macros/s/AKfycbz1Ggu7kyUmiGOwYjbwFQrJeFtXfmkETt0s_rfD278eLync2_sC071Eh6mN9_HpYMYeWw/exec?fullname=' + fullname)
       .then(async response => {
          let priviousQuery = response.data.data[0].reqandres;
-         console.log(priviousQuery)
-         let Reference_document =
-            `Note that: that the reponse must be less then 200 character\n  personal info\nName- Angelica Palacio.\nGmail- angelicapalacioshomes@gmail.com.\nWork in- Real Estate Agent.\n\nBusiness Info\nPurpose: Helping people buy and sell houses\n\nRequirements\nDown Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.\nCredit Score- A credit score of 580 or higher is required.\nEmployment History- A minimum of 2 years of employment is required.\n\nAvailable Area for buying and selling home:\n1-Riverside County\n2-San Diego County\n3-Los Angeles County\n4- San Bernardino County\n\nRequirementsDown Payment- The first-time home buyer program you mentioned requires a 3.5% down payment.\nCredit Score- A credit score of 580 or higher is required.\nEmployment History- A minimum of 2 years of employment is required.\n\nschedule a call with me.\nhttps://calendly.com/angelicapalacioshomes/15min \n\nconversation history [${priviousQuery}]\n\ncustomer query "${question}".  just gave the reply.`
+
+         // let Reference_document =
+         //    `personal info:
+         //    Name Angelica Palacio.
+         //    Gmail angelicapalacioshomes@gmail.com.
+         //    Work in Real Estate Agent.
+            
+         //    Business Info:
+         //    Purpose Helping people buy and sell houses
+            
+         //    Requirements:
+         //    The first-time home buyer program you mentioned requires a 3.5% down payment.
+         //    A credit score of 580 or higher is required.
+         //    A minimum of 2 years of employment is required.
+            
+            
+         //    Available Area for buying and selling home:
+         //    1 Riverside County
+         //    2 San Diego County
+         //    3 Los Angeles County
+         //    4 San Bernardino County
+            
+         //   schedule a call with me.
+         //   https://calendly.com/angelicapalacioshomes/15min
+           
+         //   conversation history: [${priviousQuery}]
+           
+         //   customer query: "${question}".  just gave the reply.`
+
+
+         let Reference_document = 
+         `Category: Personal Info
+         Name: Angelica Palacio
+         Gmail: angelicapalacioshomes@gmail.com
+         Work: Real Estate Agent
+         
+         Category: Business Info
+         Purpose: Helping people buy and sell houses
+         
+         Category: Requirements
+         
+         3.5% down payment is required for the first-time home buyer program.
+         A credit score of 580 or higher is required.
+         A minimum of 2 years of employment is required.
+         Category: Available Area for Buying and Selling Homes
+         
+         Riverside County
+         San Diego County
+         Los Angeles County
+         San Bernardino County
+         Schedule a call with me:
+         
+         Link: [https://calendly.com/angelicapalacioshomes/15min]
+         Conversation History:
+         [${priviousQuery}]
+         
+         Customer Query: "${question}" (just gave the reply)`
 
 
          const configuration = new Configuration({
@@ -56,7 +110,6 @@ app.get('/', async (req, res) => {
             response2 = text.substr(255, 255);
             response3 = text.substr(510, 255);
          }
-         console.log({ response1: response1, response2: response2, response3: response3 })
          res.json({ response1: response1, response2: response2, response3: response3 })
       })
       .catch(error => {
